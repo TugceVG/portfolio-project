c5eed264fe00d4bc26aec4a88f0bd226b66e926b
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-05-03 23:10:28 +0200

    feat: add new packages

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,29 @@
 import "./topbar.scss";
+import { Person, Mail } from "@mui/icons-material";
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
     return (
-        <div className="topbar">
-
+        <div className={"topbar " + (menuOpen && "active")}>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">logo</a>
+                    <div className="itemContainer">
+                        <Person className="icon" />
+                        <span>+352 691 171 791</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon" />
+                        <span>tugcevatansever91@gmail.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
