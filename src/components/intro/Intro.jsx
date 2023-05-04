e85651ffad26c94c05a8d484e516da9bc5116742
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-05-04 22:53:58 +0200

    chore: update intro comp and topbar css

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,24 @@
 import "./intro.scss";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 export default function Intro() {
     return (
-        <div className="intro">
-
+        <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/photo.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There I'm</h2>
+                    <h1>Tugce Vatansever Gunduz</h1>
+                    <h3>Junior <span>Front-End Developer</span></h3>
+                </div>
+                <a href="#portfolio">
+                    <KeyboardArrowDownIcon />
+                </a>
+            </div>
         </div>
     )
 }
