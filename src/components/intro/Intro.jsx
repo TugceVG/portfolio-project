4ffc90d84a349eba5f7ca14623df37c373763026
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-05-05 23:32:28 +0200

    feat: add dependency and update intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,21 @@
 import "./intro.scss";
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+
+    const textRef = useRef();
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: false,
+            strings: ['JavaScript Developer', 'Front-End Developer'],
+            startDelay: 600,
+            backDelay: 2000,
+            backSpeed: 70,
+        });
+    }, [])
+
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -13,10 +27,10 @@ export default function Intro() {
                 <div className="wrapper">
                     <h2>Hi There I'm</h2>
                     <h1>Tugce Vatansever Gunduz</h1>
-                    <h3>Junior <span>Front-End Developer</span></h3>
+                    <h3>Junior <span ref={textRef}></span></h3>
                 </div>
                 <a href="#portfolio">
-                    <KeyboardArrowDownIcon />
+                    <KeyboardArrowDownIcon className="arrow" />
                 </a>
             </div>
         </div>
