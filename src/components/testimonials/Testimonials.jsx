5d9e42a708f9612fb7652967537cef332694c392
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-05-10 22:30:20 +0200

    chore: render Testimonials from the data

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,24 +1,59 @@
 import "./testimonials.scss";
 
 export default function Testimonials() {
+    const data = [
+        {
+            id: 1,
+            name: "Tom Durden",
+            title: "Senior Developer",
+            img:
+                "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+            icon: "assets/twitter.png",
+            desc:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.",
+        },
+        {
+            id: 2,
+            name: "Alex Kalinski",
+            title: "Co-Founder of DELKA",
+            img:
+                "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+            icon: "assets/youtube.png",
+            desc:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ",
+            featured: true,
+        },
+        {
+            id: 3,
+            name: "Martin Harold",
+            title: "CEO of ALBI",
+            img:
+                "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+            icon: "assets/linkedin.png",
+            desc:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
+        },
+    ];
     return (
         <div className="testimonials" id="testimonials">
             <h1>Testimonials</h1>
             <div className="container">
-                <div className="card">
-                    <div className="top">
-                        <img src="assets/right-arrow.png" className="left" alt="" />
-                        <img src="https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className="user" alt="" />
-                        <img src="assets/youtube.png" className="right" alt="" />
+                {data.map((dataItem) => (
+                    <div className={dataItem.featured ? "card featured" : "card"}>
+                        <div className="top">
+                            <img src="assets/right-arrow.png" className="left" alt="" />
+                            <img src={dataItem.img} className="user" alt="" />
+                            <img src={dataItem.icon} className="right" alt="" />
+                        </div>
+                        <div className="center">
+                            {dataItem.desc}
+                        </div>
+                        <div className="bottom">
+                            <h3>{dataItem.name}</h3>
+                            <h4>{dataItem.title}</h4>
+                        </div>
                     </div>
-                    <div className="center">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis doloremque ea ratione eos nostrum.
-                    </div>
-                    <div className="bottom">
-                        <h3>Alex</h3>
-                        <h4>CEO of XYZ Company</h4>
-                    </div>
-                </div>
+                ))}
             </div>
 
         </div>
