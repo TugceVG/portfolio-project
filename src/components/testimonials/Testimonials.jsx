cdab740e12abb05558ab5145753b5398ccbdcbcb
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-06-07 00:02:55 +0200

    chore: update testimonials with real data

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,61 +1,79 @@
+import { useEffect, useState } from "react";
+
+import Form from "../form/Form";
 import "./testimonials.scss";
 
+const icon = "assets/twitter.png";
+const img = "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
+
 export default function Testimonials() {
-    const data = [
-        {
-            id: 1,
-            name: "Tom Durden",
-            title: "Senior Developer",
-            img:
-                "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-            icon: "assets/twitter.png",
-            desc:
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.",
-        },
-        {
-            id: 2,
-            name: "Alex Kalinski",
-            title: "Co-Founder of DELKA",
-            img:
-                "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-            icon: "assets/youtube.png",
-            desc:
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ",
-            featured: true,
-        },
-        {
-            id: 3,
-            name: "Martin Harold",
-            title: "CEO of ALBI",
-            img:
-                "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-            icon: "assets/linkedin.png",
-            desc:
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
-        },
-    ];
+    const [testimonials, setTestimonials] = useState([]);
+    const [isFormVisible, setIsFormVisible] = useState(false);
+    const [shouldUpdate, setShouldUpdate] = useState(false);
+
+    const toggleForm = () => {
+        setIsFormVisible((isFormVisible) => !isFormVisible)
+    }
+
+    useEffect(() => {
+        const getAllTestimonials = async () => {
+            try {
+                const res = await fetch("/testimonials/all");
+                const data = await res.json();
+
+                setTestimonials(data);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        getAllTestimonials();
+    }, [])
+
+    useEffect(() => {
+        if (shouldUpdate) {
+            const getAllTestimonials = async () => {
+                try {
+                    const res = await fetch("/testimonials/all");
+                    const data = await res.json();
+
+                    setTestimonials(data);
+                } catch (err) {
+                    console.log(err);
+                }
+            }
+
+            getAllTestimonials();
+        }
+    }, [shouldUpdate]);
+
     return (
         <div className="testimonials" id="testimonials">
-            <h1>Testimonials</h1>
-            <div className="container">
-                {data.map((dataItem) => (
-                    <div className={dataItem.featured ? "card featured" : "card"}>
-                        <div className="top">
-                            <img src="assets/right-arrow.png" className="left" alt="" />
-                            <img src={dataItem.img} className="user" alt="" />
-                            <img src={dataItem.icon} className="right" alt="" />
-                        </div>
-                        <div className="center">
-                            {dataItem.desc}
-                        </div>
-                        <div className="bottom">
-                            <h3>{dataItem.name}</h3>
-                            <h4>{dataItem.title}</h4>
-                        </div>
-                    </div>
-                ))}
+            <div className="testimonialsTitle">
+                <h1>Testimonials</h1>
+                <button onClick={toggleForm}>Add a comment</button>
             </div>
-
+            <div className="container">
+                {isFormVisible
+                    ? <Form toggleForm={toggleForm} setShouldUpdate={setShouldUpdate} />
+                    : testimonials.filter((testimonial) => testimonial.isVisible).map((testimonial) => (
+                        <div className={testimonial.featured ? "card featured" : "card"}>
+                            <div className="top">
+                                <img src="assets/right-arrow.png" className="left" alt="" />
+                                <img src={img} className="user" alt="" />
+                                <img src={icon} className="right" alt="" />
+                            </div>
+                            <div className="center">
+                                {testimonial.comment}
+                            </div>
+                            <div className="bottom">
+                                <h3>{testimonial.userName}</h3>
+                                <h4>{testimonial.userTitle}</h4>
+                                <h5>{testimonial.userCompany}</h5>
+                            </div>
+                        </div>
+                    ))}
+            </div>
         </div>
     )
 }
