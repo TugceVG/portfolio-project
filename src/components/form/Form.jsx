ff48a0d13af7f17b336f9158a027acaa8686c93b
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-06-07 23:39:27 +0200

    feat: add Form component for testimonials, update gitignore

---
+++ src/components/form/Form.jsx
@@ -0,0 +1,71 @@
+import { useRef, useState } from 'react';
+
+import "./form.scss";
+
+export default function Form({ toggleForm, setShouldUpdate }) {
+    const formRef = useRef(null);
+    const [file, setFile] = useState(null);
+
+    const saveTestimonial = async (data) => {
+        try {
+            await fetch("/testimonials/add", {
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: { 'Content-Type': 'application/json' }
+            });
+            formRef.current.reset();
+            setShouldUpdate(true);
+            toggleForm();
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const { picture, username, userTitle, userCompany, comment } = formRef.current;
+        const formData = {
+            userName: username.value,
+            userTitle: userTitle.value,
+            userCompany: userCompany.value,
+            userPicture: picture.value,
+            comment: comment.value,
+            isVisible: false,
+        };
+
+        if (file) {
+            const data = new FormData();
+            const fileName = Date.now() + file.name;
+            data.append("name", fileName);
+            data.append("file", file);
+            formData.userPicture = fileName;
+
+            try {
+                await fetch("/upload", {
+                    method: "POST",
+                    body: JSON.stringify(data),
+                    headers: { 'Content-Type': 'application/json' }
+                });
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        saveTestimonial(formData);
+    }
+
+    return (
+        <form ref={formRef} onSubmit={handleSubmit} className="form">
+            <label htmlFor="picture">Profile Picture</label>
+            <input id="picture" name="picture" type="file" accept=".png,.jpeg,.jpg" placeholder="User Picture" onChange={(e) => setFile(e.target.files[0])} />
+            <label htmlFor="username">Username</label>
+            <input id="username" name="username" type="text" placeholder="Name" />
+            <label htmlFor="userTitle">Title</label>
+            <input id="userTitle" name="userTitle" type="text" placeholder="Title" />
+            <label htmlFor="userCompany">Company</label>
+            <input id="userCompany" name="userCompany" type="text" placeholder="Company" />
+            <label htmlFor="comment">Comment</label>
+            <textarea id="comment" name="comment" placeholder="Comment" />
+            <input type="submit" placeholder="Submit" />
+        </form>
+    )
+}
